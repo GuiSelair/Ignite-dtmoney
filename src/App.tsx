db8f690c90dfb05b407a1c8ba90a0f18f7b03fcc
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-06-08 21:19:07 -0300

    Feat: Added GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import GlobalStyles from "./styles/GlobalStyles";
 
 export function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <GlobalStyles/>
+      <h1>Hello</h1>
+    </>
   );
 }
 
