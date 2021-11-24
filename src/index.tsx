f808f74e649a74371d27be4da17408c90ffa7b05
Guilherme Selair <guilherme.lima1997@hotmail.com>
2021-11-23 23:40:43 -0300

    Feat: Added mirage database mock

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
-  routes() {
-    this.namespace = "api";
-    this.get('/transactions', () => {
-      return [
+  models: {
+    transaction: Model
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Transaction 1',
-          amout: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date(),
+          title: "Freelance de website",
+          type: "deposit",
+          amount: 6000,
+          category: "Dev",
+          createdAt: new Date("2021-02-12 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Compra de patinete",
+          type: "withdraw",
+          amount: 1200,
+          category: "Compras",
+          createdAt: new Date("2021-03-02 15:00:00")
         }
       ]
+    })
+  },
+  routes() {
+    this.namespace = "api";
+    
+    this.get('/transactions', () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
     })
   }
 });
